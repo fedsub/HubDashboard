d307a1fa6a839d47cc0abe05e23e5847fc03c3b2
Goef <geoffrey0807@hotmail.com>
2018-05-21 15:18:39 +0200

    mapsjs

---
+++ app/js/maps.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyB6I4m0PabqJB2oAEJoatzYbNpnsUFhJsY'
+
+  });
+  function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+}
