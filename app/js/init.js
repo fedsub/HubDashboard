6ffc2b08a9c91c7e989a708ed34fe148daaf5d12
jfpires <jerssonpires@gmail.com>
2018-05-29 16:53:14 +0200

    edit logo navbar

--- app/js/init.js
+++ app/js/init.js
@@ -1,8 +1,13 @@
-(function($){
-    $(function(){
+// (function($){
+//     $(function(){
   
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
+//       $('.sidenav').sidenav();
+//       $('.parallax').parallax();
   
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
+//     }); // end of document ready
+//   })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
