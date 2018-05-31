d6d05771cfe6adc33325b60d9c70afdfe2585f4c
jfpires <jerssonpires@gmail.com>
2018-05-31 16:59:04 +0200

    added sort and changed nav

--- app/js/addressDataLoader.js
+++ app/js/addressDataLoader.js
@@ -16,9 +16,13 @@ let dataAddress;
             let cell5 = row.insertCell(4);
             let cell6 = row.insertCell(5);
             let cell7 = row.insertCell(6);
-                cell1.innerHTML = "<b>Naam</b>";
-                cell2.innerHTML = "<b>Adres</b>";
-                cell3.innerHTML = "<b>Postcode</b>";
+                // Auto sort the table by clicking the table headers
+                cell1.addEventListener("click", function(){sortTable(0)})
+                cell2.addEventListener("click", function(){sortTable(1)})
+                cell3.addEventListener("click", function(){sortTable(2)})
+                cell1.innerHTML = "<b>Naam</b><i class='tiny center material-icons'>sort_by_alpha</i>";
+                cell2.innerHTML = "<b>Adres</b><i class='tiny center material-icons'>sort_by_alpha</i>";
+                cell3.innerHTML = "<b>Postcode</b><i class='tiny center material-icons'>sort_by_alpha</i>";
                 cell4.innerHTML = "<b>Telefoonnummer</b>";
                 cell5.innerHTML = "<b>ID</b>";
                 cell6.innerHTML = "<b>Aanpassen</b>";
