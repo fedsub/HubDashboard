19c8cfc95f1e9796680cfe7cf078bb0b7e7a4982
Goef <geoffrey0807@hotmail.com>
2018-06-05 10:43:58 +0200

    Fix lel

--- app/js/postNewPackage.js
+++ app/js/postNewPackage.js
@@ -1,4 +1,19 @@
+// Post new address into api
 function doSomething() {
-    alert('Form submitted!');
-    return false;
+    let xhr = new XMLHttpRequest();
+    let url = "http://167.99.222.47:8000/api/shipments";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({     "name": fullname.value,
+                                    "address": address.value,
+                                    "zip": zipcode.value,
+                                    "zipNumbers": zipcode.value.replace(/\D/g,''),
+                                    "phone": phone.value});
+        xhr.send(data);
+        return false;
 }
